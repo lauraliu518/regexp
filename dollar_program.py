ce11cc358defea9a7cda9602384cca76e399cd65
lauraliu518 <yixuanliu0518@gmail.com>
2025-02-04 17:21:59 -0500

    final version

--- dollar_program.py
+++ dollar_program.py
@@ -1,32 +1,25 @@
 import re
 
-# count = 0
 inputFileName = input()
 inputFile = open(inputFileName, "r")
 content = inputFile.read()
 outputFile = open("dollar_output.txt", "w")
 
 pattern = re.compile(r"""
-                        (?<!\S) 
-                        (   
-                            \$\d{1,3}(?:,\d{3})*(?:\.\d{1,2})?  #$6.57, $10,000.99
-                            |
-                            \$?\d{1,3}(?:,\d{3})*(?:\.\d{1,2})?\s*(million|billion|trillion)?\s*(dollars?|USD)  # 500 million dollars, 1.5 billion USD
-                            |
-                            \d+\s*dollars?(?:\s+and\s+\d+\s+cents?)?  # 1 dollar and 7 cents
-                            |
-                            \d+\s*cents?  # 75 cents, 1 cent
-                            |
-                            \$?(?!hundreds?|thousands?|millions?|billions?)\b\w+(\s(?:hundreds?|thousands|millions|billions|of))*\s(?:USD|dollars?|cents?)\b #five million dollar, 
-                        ) 
-                        (?!\S)  
+                        \$\d{1,3}(?:,\d{3})*(?:\.\d{1,2})?  #$6.57, $10,000.99
+                        |
+                        \$?\d{1,3}(?:,\d{3})*(?:\.\d{1,2})?\s*(million|billion|trillion)?\s*(dollars?|USD)  # 500 million dollars, 1.5 billion USD
+                        |
+                        \d+\s*dollars?(?:\s+and\s+\d+\s+cents?)?  # 1 dollar and 7 cents
+                        |
+                        \d+\s*cents?  # 75 cents, 1 cent
+                        |
+                        \$?(?!hundreds?|thousands?|millions?|billions?)\b\w+(\s(?:hundreds?|thousands|millions|billions|of))*\s(?:USD|dollars?|cents?)\b #five million dollar,  
                     """, re.IGNORECASE | re.VERBOSE)
 matches = pattern.finditer(content)
 
 for m in matches:
     outputFile.write(m.group(0)+"\n")
-    # count += 1
-    # print(m.group(0))
 
 inputFile.close()
 outputFile.close()
--- telephone_regexp.py
+++ telephone_regexp.py
@@ -5,8 +5,6 @@ outputFile = open("telephone_output.txt", "w")
 inputFile = open(inputFileName, "r")
 inputContent = inputFile.read()
 
-regex4 = re.compile(r'[0-9]{3}-[0-9]{3}-[0-9]{4}|[0-9]{3}-[0-9]{4}')
-
 
 pattern = re.compile(r"""
                         \b\d{3}[-.]?\d{3}[-.]?\d{4}\b #standard 10 digit xxx.xxx.xxxx or xxx-xxx-xxxx or xxxxxxxxxx
@@ -19,13 +17,8 @@ pattern = re.compile(r"""
                     """, re.IGNORECASE | re.VERBOSE)
 
 matches = pattern.finditer(inputContent)
-count = 0
 for m in matches:
     outputFile.write(m.group(0)+"\n")
-    count += 1
-    # print(m.group(0))
-print(count)
-
 
 inputFile.close()
 outputFile.close()
